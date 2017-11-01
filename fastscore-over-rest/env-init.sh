eec146752334d1a801ca3e24b8cc61126c60c5b2
YevGitHub <jeka.menaker@gmail.com>
2017-11-01 14:40:38 -0700

    Update env-init.sh

--- fastscore-over-rest/env-init.sh
+++ fastscore-over-rest/env-init.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/YevGitHub/fastscore-over-rest
 docker volume create --name=db
-docker-compose -f fastscore-over-rest/docker-compose.yml up -d
+# docker-compose -f fastscore-over-rest/docker-compose.yml up -d
 chmod ugo+x fastscore-over-rest/install-fastscore-cli.sh
 apt install -y python-pip
 fastscore-over-rest/install-fastscore-cli.sh 1.6
+
