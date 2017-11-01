2a4ec8a2a2909d633c12577811135ddb5e5bb88f
Scrapbook Git Tutorial <git-tutorial@joinscrapbook.com>
2017-11-01 19:45:48 +0000

    Initial commit

---
+++ fastscore-over-rest/env-init.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/YevGitHub/fastscore-over-rest
+pip install docker-compose
+docker volume create --name=db
+docker-compose -f fastscore-over-rest/docker-compose.yml up -d
+chmod ugo+x fastscore-over-rest/install-fastscore-cli.sh
+fastcore-over-rest/install-fastscore-cli.sh 1.6
---
+++ fastscore-over-rest/step1.md
@@ -0,0 +1,14 @@
+List FastScore configuration:
+
+`cat config.yaml`{{execute}}
+
+Connect to FastScore:
+
+`fastscore connect https://127.0.0.1:8000`{{execute}}
+
+Configure FastScore:
+
+`fastscore config set config.yaml`{{execute}}
+
+Show FastScore fleet status
+`fastscore fleet -v`{{execute}}
