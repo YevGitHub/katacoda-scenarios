62440585f3ba8084d50688282854a9f831991dd2
Yevgeny Menaker <yev.menaker@opendatagroup.com>
2017-11-19 16:50:04 -0800

    Add a few packages

--- fastscore-over-rest/env-init.sh
+++ fastscore-over-rest/env-init.sh
@@ -2,6 +2,9 @@ git clone https://github.com/YevGitHub/fastscore-over-rest
 docker volume create --name=db
 # docker-compose -f fastscore-over-rest/docker-compose.yml up -d
 chmod ugo+x fastscore-over-rest/install-fastscore-cli.sh
-apt install -y python python-pip
+apt install -y libyaml-dev python python-pip
+pip install -U pip
+pip install -U urllib3==1.21.1
+pip install -U idna==2.5
 fastscore-over-rest/install-fastscore-cli.sh 1.6.1
 
