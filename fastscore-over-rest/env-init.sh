98b27dc6df3ad01f16c68e9aa9a1b99cb1e3aa35
Yevgeny Menaker <yev.menaker@opendatagroup.com>
2017-11-19 17:06:46 -0800

    removing pip commands

--- fastscore-over-rest/env-init.sh
+++ fastscore-over-rest/env-init.sh
@@ -2,9 +2,5 @@ git clone https://github.com/YevGitHub/fastscore-over-rest
 docker volume create --name=db
 # docker-compose -f fastscore-over-rest/docker-compose.yml up -d
 chmod ugo+x fastscore-over-rest/install-fastscore-cli.sh
-apt install -y python python-pip
-pip install -U pip
-pip install -U urllib3==1.21.1
-pip install -U idna==2.5
 fastscore-over-rest/install-fastscore-cli.sh 1.6.1
 
