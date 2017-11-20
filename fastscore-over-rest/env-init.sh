2f0dafa9c303ce880a0ed53db4ace7250e575a28
Yevgeny Menaker <yev.menaker@opendatagroup.com>
2017-11-19 16:42:36 -0800

    Install Python

--- fastscore-over-rest/env-init.sh
+++ fastscore-over-rest/env-init.sh
@@ -2,6 +2,6 @@ git clone https://github.com/YevGitHub/fastscore-over-rest
 docker volume create --name=db
 # docker-compose -f fastscore-over-rest/docker-compose.yml up -d
 chmod ugo+x fastscore-over-rest/install-fastscore-cli.sh
-apt install -y python-pip
+apt install -y python python-pip
 fastscore-over-rest/install-fastscore-cli.sh 1.6.1
 
